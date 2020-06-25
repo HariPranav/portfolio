7abb2b2f44e18ac366212a1c2e6191672cd7768b
Hari Pranav <haripranav98@gmail.com>
2020-06-25 21:37:47 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "56e76b227f2c16992847d7fc617f9448",
+  "assets/AssetManifest.json": "97c2fea4295d5f669ab9a9ef2209550f",
 "assets/assets/bb.png": "838b770c2db72833be7247f328c019cf",
 "assets/assets/blog.png": "599e0917cccb146774c07bf98488c216",
 "assets/assets/facebook-round-color.png": "29e884d5b638bc3f46801a2be2fd7e1a",
@@ -11,6 +11,7 @@ const RESOURCES = {
 "assets/assets/HP.png": "7e7babad6223403d168a9ae596ea22aa",
 "assets/assets/icons8-hacker-64.png": "31a5a70e9bb76ed679c9f6841573cc6e",
 "assets/assets/linkedin-square-color.png": "c8b3614447a287a5251f6e3f80be1fb2",
+"assets/assets/medium.png": "a6b7120c1ed2628e53a8a0841ed59ed8",
 "assets/assets/Profile1.png": "c40f686558291c21314ad58382ab30ef",
 "assets/assets/Profile2.png": "fbd8ade67028084fa8df1539666d7390",
 "assets/assets/Profile3.png": "34cc2b4a07aecc9337905c058fcb7655",
@@ -21,7 +22,7 @@ const RESOURCES = {
 "assets/assets/youtube-color.png": "6cd8ec3bac97870dbf0fcfbcd3513930",
 "assets/FontManifest.json": "d1d98195c74ea8578011539c85a1ba98",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "d3245857cae0e01b278bb5200b334ca6",
+"assets/NOTICES": "33202baae1a93687e6d601bfb8e17c52",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/packages/flutter_auth_buttons/fonts/Roboto-Medium.ttf": "d08840599e05db7345652d3d417574a9",
 "assets/packages/flutter_auth_buttons/fonts/SF-Pro-Medium.ttf": "8b61dea99518b51d5511dc3963a5537c",
@@ -36,7 +37,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2def70cdfb9efbc99017bc85e0c5e804",
 "/": "2def70cdfb9efbc99017bc85e0c5e804",
-"main.dart.js": "9460404662cc698a1133aa007eed5488",
+"main.dart.js": "82899bf69b8072f147a450e684484d3a",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210"
 };
 
@@ -151,7 +152,7 @@ self.addEventListener("fetch", (event) => {
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
-  if (event.message == 'skipWaiting') {
+  if (event.data == 'skipWaiting') {
     return self.skipWaiting();
   }
 
